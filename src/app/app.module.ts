5f36cb000b9bb57aedf84eff16584c896465f401
Valentin Coppin <valentin@socode.fr>
2020-07-17 10:20:53 +0200

    Clean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  // providers: [NumberPeopleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
